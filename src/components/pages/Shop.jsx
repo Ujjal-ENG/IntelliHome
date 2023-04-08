f2859f9994040eed8ae0898452152deb61b84c55
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-08 20:51:17 +0600

    added the handle cart item for track the cart items

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -3,11 +3,15 @@ import { useLoaderData } from 'react-router-dom';
 import SingleProducts from './SingleProducts';
 const Shop = () => {
     const data = useLoaderData();
-    console.log(data);
+
+    // handle Cart Item
+    const handleCartItem = (id) => {
+        console.log(id);
+    };
     return (
         <div className="mt-8 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-items-center gap-6">
             {data.map((el) => (
-                <SingleProducts key={el.id} data={el} />
+                <SingleProducts key={el.id} data={el} handleCartItem={handleCartItem} />
             ))}
         </div>
     );
--- src/components/pages/SingleProducts.jsx
+++ src/components/pages/SingleProducts.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SingleProducts = ({ data }) => {
+const SingleProducts = ({ data, handleCartItem }) => {
     const { id, category, name, picture, price } = data;
     return (
         <div className="bg-gray-100 p-6 rounded shadow-lg">
@@ -8,7 +8,7 @@ const SingleProducts = ({ data }) => {
             <p className="mb-2 text-xl font-bold leading-none sm:text-2xl">{name}</p>
             <p className="text-gray-700 ">Category: {category}</p>
             <p className="text-gray-700 font-bold">Price: {price}$</p>
-            <button onClick={() => alert('Item added in the cart')} type="button" className="btn-primary w-full block">
+            <button onClick={() => handleCartItem(id)} type="button" className="btn-primary w-full block">
                 Add To Cart
             </button>
         </div>
