b2a2ffb37b80dec3a39a99f415d61c4e96e99c6d
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-08 18:57:15 +0600

    done the design of the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layouts/Navbar';
 
 function App() {
-    return <div className="app"></div>;
+    return (
+        <div className="">
+            <Navbar />
+            <Outlet />
+        </div>
+    );
 }
 
 export default App;
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,6 +1,57 @@
 import React from 'react';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { GrTechnology } from 'react-icons/gr';
+import { NavLink } from 'react-router-dom';
 const Navbar = () => {
-    return <></>;
+    return (
+        <>
+            <nav className="px-24 py-8 flex justify-between items-center shadow-md w-full">
+                <div>
+                    <NavLink
+                        className={(isActive) => {
+                            return isActive ? 'active' : 'default';
+                        }}>
+                        <span className="flex items-center text-3xl">
+                            <GrTechnology />
+                            IntelliHome
+                        </span>
+                    </NavLink>
+                </div>
+
+                <div className="flex  items-center gap-6">
+                    <NavLink
+                        className={(isActive) => {
+                            return isActive ? 'active' : 'default';
+                        }}>
+                        <span className="text-3xl">Home</span>
+                    </NavLink>
+
+                    <NavLink
+                        className={(isActive) => {
+                            return isActive ? 'active' : 'default';
+                        }}>
+                        <span className="text-3xl">Shop</span>
+                    </NavLink>
+
+                    <NavLink
+                        className={(isActive) => {
+                            return isActive ? 'active' : 'default';
+                        }}>
+                        <span className="text-3xl">
+                            <AiOutlineShoppingCart />
+                        </span>
+                    </NavLink>
+
+                    <NavLink
+                        className={(isActive) => {
+                            return isActive ? 'active' : 'default';
+                        }}>
+                        <span className="text-3xl">About</span>
+                    </NavLink>
+                </div>
+            </nav>
+        </>
+    );
 };
 
 export default Navbar;
